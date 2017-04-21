967b81ae8b7e20fd515f4c45dabd8aa261a7600a
DESKTOP-MDM7NRG\fuzucoo <jojowasaman00@gmail.com>
2017-04-21 14:07:31 +0900

    JSPとサーブレット2　占いシステムの課題のファイルを提出します

---
+++ CampChallenge_java/004.JSPとサーブレット２/占いシステム/FortuneTelling.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Random;
+import javax.servlet.RequestDispatcher;
+import org.camp.servlet.ResultData;
+
+
+/**
+ *
+ * @author fuzucoo
+ */
+public class FortuneTelling extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        
+        final String result = "/WEB-INF/jsp/FortuneTellingResult.jsp";
+        
+        //大吉・中吉・小吉・吉・末吉・凶・大凶
+        String LuckList[] = {"大吉","中吉","小吉","吉","末吉","凶","大凶"};
+                
+        //乱数クラス作成
+        Random rand = new Random();
+        
+        Integer index = rand.nextInt(LuckList.length);
+        
+        out.println("今日のあなたの運勢は" + LuckList[index] + "です。");
+        
+        //リクエストスコープへ結果を設定
+        ResultData data = new ResultData();
+        data.setD(new Date());
+        data.setLuck(LuckList[index]);
+        request.setAttribute("DATA",data);
+        
+        RequestDispatcher rd = request.getRequestDispatcher(result);
+        rd.forward(request,response);
+        
+        
+        
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FortuneTelling</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FortuneTelling at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }finally{
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
